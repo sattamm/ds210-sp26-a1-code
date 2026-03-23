ca9fce10cc359974c6a277759165a74d7f122a5b
jiap6 <jiap8067@gmail.com>
2026-03-22 21:27:34 -0400

    v5

--- project_2_chatbot/cache_chatbot/src/solution/v5.rs
+++ project_2_chatbot/cache_chatbot/src/solution/v5.rs
@@ -23,13 +23,40 @@ impl ChatbotV5 {
         match cached_chat {
             None => {
                 println!("chat_with_user: {username} is not in the cache!");
-                // The cache does not have the chat. What should you do?
-                return String::from("Hello, I am not a bot (yet)!");
+                // since the convo is not in the cache, we can check if its in the file 
+                // try to load previous conversation 
+                let mut chat = 
+                if let Some(session) = file_library::load_chat_session_from_file(filename) {
+                    println!("Loaded {username} from file");
+                    //rebuild the chat
+                    self.model.chat().with_session(session)
+                } 
+                // no file found, new user 
+                else {
+                    println!("Creating new chat for {username}");
+                    self.model.chat()
+                };
+                // add the message to the conversation, and comes up with a response 
+                let response = chat.add_message(message).await.unwrap().to_string();
+                // save the session to the file 
+                // &chat.session.unwrap() here because mismatched types
+                file_library::save_chat_session_to_file(filename, &chat.session().unwrap());
+                //put into the cache 
+                self.cache.insert_chat(username, chat);
+                return response; 
             }
             Some(chat_session) => {
                 println!("chat_with_user: {username} is in the cache! Nice!");
-                // The cache has this chat. What should you do?
-                return String::from("Hello, I am not a bot (yet)!");
+                //  the chat is already in memory
+                let mut chat = chat_session; 
+                // add the message to the conversation, and comes up with a response 
+                let response = chat.add_message(message).await.unwrap().to_string();
+                // save the convo to the file 
+                 // save the session to the file 
+                // &chat.session.unwrap() here because mismatched types
+                file_library::save_chat_session_to_file(filename, &chat.session().unwrap());
+                self.cache.insert_chat(username, chat);
+                return response; 
 
             }
         }
