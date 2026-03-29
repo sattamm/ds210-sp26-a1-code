3d67899def868afbe3874911d7816c4477c8fefc
Sattam <sattamm@bu.edu>
2026-03-29 12:54:43 -0400

    project 3 part 1 code

--- project_3_client_server_analytics/analytics_lib/src/solution.rs
+++ project_3_client_server_analytics/analytics_lib/src/solution.rs
@@ -2,12 +2,56 @@ use std::collections::HashMap;
 use crate::dataset::{ColumnType, Dataset, Value, Row};
 use crate::query::{Aggregation, Condition, Query};
 
+pub fn check(dataset: &Dataset, row: &Row, condition: &Condition) -> bool { // this is the new function we added to return the true or false, passes by ref
+    match condition {
+        Condition::Equal(column, value) => {
+            let index = dataset.column_index(column);
+            row.get_value(index) == value
+        }
+
+        Condition::And(left, right) => {
+            check(dataset, row, left) && check(dataset, row, right) // recursively called
+        }
+
+        Condition::Or(left, right) => {
+            check(dataset, row, left) || check(dataset, row, right)
+        }
+        Condition::Not(inner) => {
+            !check(dataset, row, inner)
+        }
+        // the condition parameter has 4 options the equal, and, or, and not
+    }
+}
+
+
 pub fn filter_dataset(dataset: &Dataset, filter: &Condition) -> Dataset {
-    todo!("Implement this!");
+    let mut temp = Dataset::new(dataset.columns().clone()); // Creates a new empty dataset with the same columns as the original dataset.
+
+    for row in dataset.iter() {
+        if check(dataset, row, filter) { // calling the new function, if true it adds to filered
+            temp.add_row(row.clone()); // pass by clone because we want to write the columns we want without deleting them from the origional dataset
+        }
+    }
+
+    temp // called temp because scope is only in function
 }
 
 pub fn group_by_dataset(dataset: Dataset, group_by_column: &String) -> HashMap<Value, Dataset> {
-    todo!("Implement this!");
+    let mut temp = HashMap::new();
+
+    let column_index = dataset.column_index(group_by_column);
+    let columns = dataset.columns().clone();
+
+    for row in dataset.into_iter() {
+        let i = row.get_value(column_index).clone();
+
+        temp
+            .entry(i)// finds the hashmap slot for i
+            .or_insert(Dataset::new(columns.clone()))// giving the dataset stored there
+            .add_row(row); // adding the row to the dataset
+    }
+
+    temp
 }
 
 pub fn aggregate_dataset(dataset: HashMap<Value, Dataset>, aggregation: &Aggregation) -> HashMap<Value, Value> {
